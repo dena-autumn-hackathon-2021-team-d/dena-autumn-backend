75c54303157b888e5e73d6e7fe747ff0d63e7a10
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2021-09-18 18:28:08 +0900

    implement api of getting answers by group id

--- repository/answer.go
+++ repository/answer.go
@@ -4,4 +4,5 @@ import "github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/domain/
 
 type Answer interface {
 	Post(answer *entity.Answer) error
+	FindByGroupID(groupID string) ([]*entity.Answer, error)
 }
