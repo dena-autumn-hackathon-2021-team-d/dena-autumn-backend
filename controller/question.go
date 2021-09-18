01934ad3d18753d5cee55fdccf3ecc3ff0166151
garebare <garedaiki@gmail.com>
2021-09-18 18:45:34 +0900

    fix:question

--- controller/question.go
+++ controller/question.go
@@ -2,34 +2,33 @@ package controller
 
 import (
 	"net/http"
-
+	"github.com/labstack/gommon/log"
 	"github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/domain/entity"
 	"github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/usecase"
-	"github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/log"
 	"github.com/gin-gonic/gin"
 )
 
 type QuestionController struct {
 	uc *usecase.QuestionUsecase
+	logger *log.Logger
 }
 
-func NewQuestionController (uc *usecase.QuestionUsecase) *QuestionController{
-	return &QuestionController{uc : uc}
+func NewQuestionController (logger *log.Logger, uc *usecase.QuestionUsecase) *QuestionController{
+	return &QuestionController{uc : uc , logger: logger}
 }
 
-func(ctrl *QuestionController) Post (c gin.Context) {
-	logger := log.New()
+func(ctrl *QuestionController) Post (c *gin.Context) {
 	var question *entity.Question
 	if err := c.ShouldBindJSON(question); err != nil {
 		c.String(http.StatusInternalServerError, "failed to bind request body")
-		logger.Errorf("failed to bind request body: :%v\n", err)
+		ctrl.logger.Errorf("failed to bind request body: :%v\n", err)
 		return
 	}
 
 	resQuestion, err := ctrl.uc.Post(question)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "failed to post answer")
-		logger.Errorf("failed to post qestion: :%v\n", err)
+		ctrl.logger.Errorf("failed to post qestion: :%v\n", err)
 		return
 	}
 
