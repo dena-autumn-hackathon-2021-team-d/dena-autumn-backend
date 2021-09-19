36d0e7c03e251657f72d058eea94c1149255ee81
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2021-09-19 10:24:16 +0900

    implement test of answer and fix answer controller

---
+++ controller/answer_test.go
@@ -0,0 +1,84 @@
+package controller_test
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/controller"
+	"github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/domain/entity"
+	"github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/log"
+	"github.com/dena-autumn-hackathon-2021-team-d/dena-autumn-backend/usecase"
+)
+
+type fakeAnswerRepository struct {
+	postFunc          func(answer *entity.Answer) error
+	findByGroupIDFunc func(groupID string) ([]*entity.Answer, error)
+	findUniqueFunc    func(groupID string, questionID, answerID int) (*entity.Answer, error)
+	findByQuestion    func(groupID string, questionID int) ([]*entity.Answer, error)
+}
+
+func (a *fakeAnswerRepository) Post(answer *entity.Answer) error {
+	return a.postFunc(answer)
+}
+
+func (a *fakeAnswerRepository) FindByGroupID(groupID string) ([]*entity.Answer, error) {
+	return a.findByGroupIDFunc(groupID)
+}
+
+func (a *fakeAnswerRepository) FindUnique(groupID string, questionID, answerID int) (*entity.Answer, error) {
+	return a.findUniqueFunc(groupID, questionID, answerID)
+}
+
+func (a *fakeAnswerRepository) FindByQuestion(groupID string, questionID int) ([]*entity.Answer, error) {
+	return a.findByQuestion(groupID, questionID)
+}
+
+func TestAnswer_GetByGroupID(t *testing.T) {
+	wantAnswers := []*entity.Answer{
+		{
+			ID:         1,
+			GroupID:    "GROUP_ID",
+			QuestionID: 2,
+			Contents:   "TEST_CONTENTS1",
+			Username:   "TEST_USERNAME1",
+			CreatedAt:  time.Date(2021, time.September, 18, 1, 0, 0, 0, time.UTC).Format(time.RFC3339),
+		},
+		{
+			ID:         3,
+			GroupID:    "GROUP_ID",
+			QuestionID: 4,
+			Contents:   "TEST_CONTENTS2",
+			Username:   "TEST_USERNAME2",
+			CreatedAt:  time.Date(2021, time.September, 18, 2, 0, 0, 0, time.UTC).Format(time.RFC3339),
+		},
+	}
+
+	answerRepo := &fakeAnswerRepository{
+		findByGroupIDFunc: func(groupID string) ([]*entity.Answer, error) {
+			return wantAnswers, nil
+		},
+	}
+	answerUC := usecase.NewAnswerUseCase(answerRepo)
+	answerCtrl := controller.NewAnswerController(log.New(), answerUC)
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	c.Params = append(c.Params, gin.Param{Key: "group_id", Value: "GROUP_ID"})
+
+	answerCtrl.GetByGroupID(c)
+
+	var gotAnswers []*entity.Answer
+	if err := json.Unmarshal(w.Body.Bytes(), &gotAnswers); err != nil {
+		t.Fatalf("failed to unmarshal: %v", err)
+	}
+
+	if diff := cmp.Diff(wantAnswers, gotAnswers); diff != "" {
+		t.Fatal(diff)
+	}
+}
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.5
 	github.com/google/uuid v1.3.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
