23cc94e6f227670886e1d712b25d371d37c796d7
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-09-19 14:17:25 +0900

    go fmt

--- domain/entity/comment.go
+++ domain/entity/comment.go
@@ -5,7 +5,7 @@ import "github.com/google/uuid"
 type Comment struct {
 	ID         string `json:"id" db:"id"`
 	GroupID    string `json:"group_id" db:"group_id"`
-	QuestionID string  `json:"question_id" db:"question_id"`
+	QuestionID string `json:"question_id" db:"question_id"`
 	AnswerID   string `json:"answer_id" db:"answer_id"`
 	Contents   string `json:"contents" db:"contents"`
 	Username   string `json:"username" db:"username"`
